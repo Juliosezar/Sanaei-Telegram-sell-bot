43afaa1c01e2f71ba29e9c302f1fe7a75d4149cd
Sina Aliee <julio.sezar.1382@gmail.com>
2024-06-19 13:57:47 +0330

    test git

--- NpasvBot/settings.py
+++ NpasvBot/settings.py
@@ -159,3 +159,5 @@ MEDIA_ROOT = environ.get('MEDIA_ROOT')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# tetsing git
