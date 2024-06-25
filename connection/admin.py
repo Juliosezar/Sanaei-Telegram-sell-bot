02a3860d76f13e1912a223763e5ef028f3c69402
Sina Aliee <julio.sezar.1382@gmail.com>
2024-06-25 13:10:53 +0330

    add EndOfConfigCounter to adminpanel

--- connection/admin.py
+++ connection/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import SendMessage
+from .models import SendMessage, EndOfConfigCounter
 
 admin.site.register(SendMessage)
+admin.site.register(EndOfConfigCounter)
